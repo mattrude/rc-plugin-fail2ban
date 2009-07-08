8920caac31ec4bbd06e971ae28a329fd8f8af557
root <root@samantha.mattrude.com>
2009-07-08 18:38:14 -0500

    Error now displays on screen, not logging yet.
    Also changed the name of the file since dashes don't seem to work
    in file names.

---
+++ fail2ban.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * RoundCube Fail2Ban Plugin
+ *
+ * @version 1.0
+ * @author Matt Rude [matt@mattrude.com]
+ * @url
+ */
+class fail2ban extends rcube_plugin
+{
+  function init()
+  {
+    $this->add_hook('login_failed', array($this, 'log'));
+  }
+
+  function log($args)
+  {
+    echo 'failed: ' .$args['user']. ' - IP Address: ' .getenv('REMOTE_ADDR'); 
+  }
+
+}
+
+?>
