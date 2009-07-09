006a7e85d58db64f6f2294477af0895ce70c02cd
Matt Rude <github@mattrude.com>
2009-07-09 00:10:14 -0500

    simple code clean up

--- fail2ban.php
+++ fail2ban.php
@@ -4,7 +4,7 @@
  *
  * @version 1.0
  * @author Matt Rude [matt@mattrude.com]
- * @url
+ * @url http://www.mattrude.com/roundcube-fail2ban-plugin
  */
 class fail2ban extends rcube_plugin
 {
@@ -15,15 +15,13 @@ class fail2ban extends rcube_plugin
 
   function log($args)
   {
-    $log_entry = 'failed login attempt - username=' .$args['user']. ' - ip-address=' .getenv('REMOTE_ADDR'); 
+    $log_entry = 'FAILED login for ' .$args['user']. ' from ' .getenv('REMOTE_ADDR'); 
     $log_config = rcmail::get_instance()->config->get('log_driver');
     
     if ($log_config == 'syslog'){
-       echo 'syslog';
        syslog(LOG_WARNING, $log_entry);
     } elseif ($log_config == 'file'){
-       echo 'file';
-       error_log(date(DATE_RFC822)." roundcube ".$log_entry."\n", 3, "logs/fail2ban.log");
+       error_log('['.date('d-M-Y H:i:s O')."]: ".$log_entry."\n", 3, "logs/userlogins");
     } else {
        echo 'WARNING!! The RoundCube Fail2Ban Plugin was unable to retrievethe log driver form the config, please check your config file for log_driver.';
     }
