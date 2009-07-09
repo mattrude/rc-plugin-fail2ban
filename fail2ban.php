cafd3371f1ece8c24bcc739a61c9e345492ccabb
Matt Rude <github@mattrude.com>
2009-07-08 23:55:33 -0500

    added file support

--- fail2ban.php
+++ fail2ban.php
@@ -16,7 +16,17 @@ class fail2ban extends rcube_plugin
   function log($args)
   {
     $log_entry = 'failed login attempt - username=' .$args['user']. ' - ip-address=' .getenv('REMOTE_ADDR'); 
-    syslog(LOG_WARNING, $log_entry);
+    $log_config = rcmail::get_instance()->config->get('log_driver');
+    
+    if ($log_config == 'syslog'){
+       echo 'syslog';
+       syslog(LOG_WARNING, $log_entry);
+    } elseif ($log_config == 'file'){
+       echo 'file';
+       error_log(date(DATE_RFC822)." roundcube ".$log_entry."\n", 3, "logs/fail2ban.log");
+    } else {
+       echo 'WARNING!! The RoundCube Fail2Ban Plugin was unable to retrievethe log driver form the config, please check your config file for log_driver.';
+    }
   }
 
 }
