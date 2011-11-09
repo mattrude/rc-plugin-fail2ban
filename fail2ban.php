21f36decd1d24fe71e3fd39ad9ba3e21e46a4eb8
Matt Rude <m@mattrude.com>
2011-11-09 07:24:26 -0600

    More small text fixes.

--- fail2ban.php
+++ fail2ban.php
@@ -24,7 +24,7 @@ class fail2ban extends rcube_plugin
     } elseif ($log_config == 'file'){
        error_log('['.date('d-M-Y H:i:s O')."]: ".$log_entry."\n", 3, "logs/userlogins");
     } else {
-       echo 'WARNING!! The RoundCube Fail2Ban Plugin was unable to retrieve the log driver form the config, please check your config file for log_driver.';
+       echo 'WARNING!! The RoundCube Fail2Ban Plugin was unable to retrieve the log driver from the config, please check your config file for log_driver.';
     }
   }
 
